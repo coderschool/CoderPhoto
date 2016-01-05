65758ec82b6dafd4110b49a7888fb0d1377e37f8
Harley Trung <harley@bsa.edu.vn>
2016-01-05 15:33:58 +0700

    add user

--- Gemfile
+++ Gemfile
@@ -46,3 +46,4 @@ group :development, :test do
 end
 
 gem 'rails_12factor', group: :production
+gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :photos do 
     collection do
       get 'index'
