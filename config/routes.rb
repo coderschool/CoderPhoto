40792ad3d4c6d2a8216e808990fef9f5ab1a49d8
Harley Trung <harley@bsa.edu.vn>
2016-01-07 14:51:45 +0700

    clean up timeline layout

--- config/routes.rb
+++ config/routes.rb
@@ -11,13 +11,11 @@ Rails.application.routes.draw do
     end
   end
 
-  root 'photos#index'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
