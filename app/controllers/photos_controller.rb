c4a0dedb3e82a1407d7d1aab13649cf21f23c4e3
Harley Trung <harley@bsa.edu.vn>
2016-01-12 17:31:00 +0700

    allow updating photo caption

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,6 +3,20 @@ class PhotosController < ApplicationController
   	@photos = Photo.all
   end
 
+  def edit
+    @photo = Photo.find params[:id]
+  end
+
+  def update
+    @photo = Photo.find params[:id]
+    photo_params = params.require(:photo).permit(:caption).merge(locale: I18n.locale)
+    if @photo.update_attributes photo_params
+      redirect_to root_path, flash: {success: "Updated"}
+    else
+      redirect_to root_path, flash: {error: "Error: #{@photo.errors.full_messages.to_sentence}"}
+    end
+  end
+
   def like
     @photo = Photo.find params[:id]
     @vote = @photo.liked_by! current_user
