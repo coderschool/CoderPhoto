cd2e0a4f2857f82e1f183a70abaed0fea0d1fbde
Harley Trung <harley@bsa.edu.vn>
2016-01-12 16:22:12 +0700

    show Time.current

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :set_locale
+  around_filter :user_time_zone, if: :current_user
 
   private
   def set_locale
@@ -16,5 +17,12 @@ class ApplicationController < ActionController::Base
     else
       I18n.locale = current_user.language
     end
+  end
+
+  private
+  def user_time_zone(&block)
+    Time.use_zone(current_user.time_zone, &block)
+    # similarly:
+    # Time.use_zone(current_user.time_zone) { yield }
   end
 end
